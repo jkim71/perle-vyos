2425bee07a6e59334edc8e4306078ea8aaec08f6
Jack Kim <jkim@perle.com>
2024-08-28 11:10:59 -0400

    240828: Update build script.
    Updated patches
    added .gitignore

--- build-perle-vyos.sh
+++ build-perle-vyos.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 CWD=$(pwd)
 
-TARGET_NAME=bookworm-am64xx-evm
-PATCH_DIR=${CWD}/patches/ti_bdebstrap
+# BASE_DIR=$(dirname ${CWD})
+BUILD_BY=$(git config user.email)
+VYOS_BUILD_DIR=vyos_build
+PATCH_DIR=${CWD}/patches
 
 set -e
 
-if [ ! -d ${PATCH_DIR} ]; then
-    echo "Patch directory does not exists"
-    exit 1
+if [ "$EUID" -ne 0 ] ; then
+    echo "Failed to run: requires root privileges"
+    echo "Exiting"
+    exit -1
 fi
 
-SRC=ti-bdebstrap
-if [ ! -d $SRC ]; then
-    echo "I: Clone TI packages"
-    git clone https://github.com/TexasInstruments/ti-bdebstrap.git $SRC
-#    echo "I: switch to branch XXX"
-#    git checkout XXX
-fi
-cd $SRC
+echo "========================================"
+echo "I: BUILD_BY       : $BUILD_BY"
+echo "I: VYOS_BUILD_DIR : $VYOS_BUILD_DIR"
+echo "I: PATCH_DIR      : $PATCH_DIR"
+echo "========================================"
 
-for patch in $(ls ${PATCH_DIR})
+if [ ! -d $VYOS_BUILD_DIR ]; then
+    echo "I: Clone VyOS build packages"
+    git clone https://github.com/vyos/vyos-build.git -b current $VYOS_BUILD_DIR
+fi
+
+cd $VYOS_BUILD_DIR
+VYOS_BUILD_PATCH=$PATCH_DIR/$VYOS_BUILD_DIR
+for patch in $(ls ${VYOS_BUILD_PATCH})
 do
-    echo "I: Apply patch: ${PATCH_DIR}/${patch}"
-    patch -p1 < ${PATCH_DIR}/${patch}
+    echo "I: Apply patch: ${VYOS_BUILD_PATCH}/${patch}"
+    patch -p1 < ${VYOS_BUILD_PATCH}/${patch}
 done
 
-echo "I: Builing BSP image for \"${TARGET_NAME}\" and creating Package"
-sudo ./build.sh ${TARGET_NAME}
+echo "I: Builing VyOS Packages"
+cp ${PATCH_DIR}/configs/arch/arm64/ti_evm_vyos_defconfig ${CWD}/${VYOS_BUILD_DIR}/linux-kernel/arch/arm64/configs/vyos_defconfig
+./build-packages.sh all
+
+echo "I: Creating VyOS Image"
+./build-image.sh $BUILD_BY $PATCH_DIR
+
+echo "I: Building BSP Images"
+./build-am64x-boot.sh $PATCH_DIR
+
+echo "I: Finished!!!"
